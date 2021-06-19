8b141f1447dd8cf639554070aa03e5295ac778ce
sankalpjadhav <sanky43jadhav@gmail.com>
2021-06-19 23:14:23 +0530

    Print in Lexicographical order.

---
+++ src/RecursionAndBacktracking/PrintInLexicographicalOrder.java
@@ -0,0 +1,22 @@
+package RecursionAndBacktracking;
+import java.util.Scanner;
+public class PrintInLexicographicalOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=9;i++){
+            dfs(i,n);
+        }
+    }
+
+    private static void dfs(int i, int n){
+        if(i>n){
+            return;
+        }
+
+        System.out.println(i);
+        for(int j=0;j<10;j++){
+            dfs(10*i + j,n);
+        }
+    }
+}
